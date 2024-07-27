9119c33a6f2c4fb47c36e04f90a4d4f5bad06916
Tiago Becker <beckertiago09@gmail.com>
2024-07-27 13:35:58 -0300

    final

--- src/app/components/utils/api.ts
+++ src/app/components/utils/api.ts
@@ -1,4 +1,4 @@
 
 
-export const authUrl = "http://localhost:443/auth";
-export const basicURL = "http://localhost:443/employee";
+export const authUrl = "http://77.37.69.19:443/auth";
+export const basicURL = "http://77.37.69.19:443/employee";
